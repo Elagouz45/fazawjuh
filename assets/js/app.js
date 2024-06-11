800de197d13e040fdc89c115b11f302aac9bb2b6
mahmoudelagouz <mahmoudelagouz21@gmail.com>
2024-06-11 03:13:41 +0300

    home design

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,11 @@ $(".js-example-basic-multiple-limit").select2({
 
 let currentStep = 0;
 const tabs = document.querySelectorAll('.progress');
+
 function showStep(step) {
     const contents = document.querySelectorAll('.info');
     contents.forEach((c, index) => {
-         activeTab(index)
+        activeTab(index)
         if (index === step) {
             c.classList.remove('d-none');
         } else {
@@ -15,7 +16,7 @@ function showStep(step) {
         }
     });
     if (step > 0) document.getElementById('prevBtn').style.display = step === 0 ? 'none' : 'inline';
-    document.getElementById('nextBtn')? document.getElementById('nextBtn').innerText = step === contents.length - 1 ? 'حفظ' : 'التالى ' : '';
+    document.getElementById('nextBtn') ? document.getElementById('nextBtn').innerText = step === contents.length - 1 ? 'حفظ' : 'التالى ' : '';
 }
 
 function activeTab(index) {
@@ -46,3 +47,23 @@ function prevStep() {
 
 // Initialize the first step
 showStep(currentStep);
+
+//tabs
+document.addEventListener("DOMContentLoaded", function () {
+    const tabButtons = Array.from(document.querySelectorAll(".tab-buttons .tab-button"));
+    const tabContents = document.querySelectorAll(".tab-content");
+    tabButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const tabId = this.getAttribute("data-tab");
+
+            // Remove active class from all tab buttons and contents
+            tabButtons.forEach(btn => btn.classList.remove("active-tab"));
+            tabContents.forEach(content => content.classList.remove("active"));
+
+            // Add active class to the clicked tab button and corresponding content
+            this.classList.add("active-tab");
+            document.getElementById(tabId).classList.add("active");
+        });
+    });
+    tabButtons[0].click();
+});
